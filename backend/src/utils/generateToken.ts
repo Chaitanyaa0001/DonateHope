255e9b3d463883d091dc5ccf985906665fc3e212
chaitanya <chaitanyakhurana0@gmail.com>
2025-08-04 01:25:18 +0530

    authentication

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -1,13 +1,55 @@
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions, JwtPayload } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET!;
-const JWT_EXPIRES_IN = '7d';
+const JWT_SECRET = process.env.JWT_SECRET as string;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
+const ACCESS_EXPIRE = process.env.ACCESS_EXPIRE || '15m';
+const REFRESH_EXPIRE = process.env.REFRESH_EXPIRE || '7d';
 
-export const generateToken = (userId: string, role: string) => {
-  return jwt.sign({ userId, role }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
-};
+
+interface TokenPayload {
+  user: string;
+  role:string
+}
+
+export const generateAccessToken =(user:string,role:string) =>{
+  const payload: TokenPayload = {user,role};
+  const options: SignOptions = {expiresIn:ACCESS_EXPIRE as SignOptions['expiresIn']}
+  return jwt.sign(payload,JWT_SECRET,options)
+}
+export const generateRefreshToken = (user:string,role:string)=>{
+  const paylaod: TokenPayload = {user,role}
+}
+
+// if (!JWT_SECRET || !JWT_REFRESH_SECRET) {
+//   throw new Error('JWT secrets are not defined in the environment variables');
+// }
+
+// interface TokenPayload {
+//   userId: string;
+//   role: string;
+// }
+
+// export const generateAccessToken = (userId: string, role: string): string => {
+//   const payload: TokenPayload = { userId, role };
+// const options: SignOptions = { expiresIn: ACCESS_EXPIRE as SignOptions['expiresIn'] };
+//   return jwt.sign(payload, JWT_SECRET, options);
+// };
+
+// export const generateRefreshToken = (userId: string, role: string): string => {
+//   const payload: TokenPayload = { userId, role };
+// const options: SignOptions = { expiresIn: ACCESS_EXPIRE as SignOptions['expiresIn'] };
+//   return jwt.sign(payload, JWT_REFRESH_SECRET, options);
+// };
+
+// export const verifyAccessToken = (token: string): TokenPayload => {
+//   return jwt.verify(token, JWT_SECRET) as TokenPayload;
+// };
+
+// export const verifyRefreshToken = (token: string): TokenPayload => {
+//   return jwt.verify(token, JWT_REFRESH_SECRET) as TokenPayload;
+// };
 
 

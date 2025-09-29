584da01407e6d8a1fa9acf0ea7ccc90b865848fa
chaitanya <chaitanyakhurana0@gmail.com>
2025-09-29 23:51:03 +0530

    git push

--- frontend/src/pages/layouts/FunderLayout.tsx
+++ frontend/src/pages/layouts/FunderLayout.tsx
@@ -1,4 +1,4 @@
-// src/layouts/FunderLayout.tsx
+
 import React from "react";
 import Sidebar from "@/components/sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
@@ -6,14 +6,12 @@ import { Outlet } from "react-router-dom";
 const FunderLayout: React.FC = () => {
   return (
     <div className="flex min-h-screen">
-      {/* Sidebar is fixed */}
       <Sidebar />
-
-      {/* Page content */}
       <div className="flex-1 lg:ml-64  min-h-screen p-3 lg:p-6">
         <Outlet /> 
       </div>
     </div>
+    
   );
 };
 

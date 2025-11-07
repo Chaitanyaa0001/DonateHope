753ef99ede6598f302a3348a5b05d6c5b7d35f3b
chaitanya <chaitanyakhurana0@gmail.com>
2025-11-07 22:01:31 +0530

    completed

--- backend/src/config/DB.ts
+++ backend/src/config/DB.ts
@@ -6,9 +6,9 @@ dotenv.config();
 export const connectDB = async () =>{
     try {
         await mongoose.connect(process.env.MONGO_URI !) 
-        console.log("Mongo DB connected");
+        console.log("Mongo DB connected via atlas");
     } catch (err) {
-        console.error("MongoDb connetction error");
+        console.error("MongoDb connetction error", err);
         
     }
 }
--- backend/src/config/redisClient.ts
+++ backend/src/config/redisClient.ts
@@ -1,39 +1,21 @@
-import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379");
+import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
-redis.on("connect", () =>{
-    console.log("Redis connected ");
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD,
 });
 
-redis.on("error" , () =>{
-    console.log("Redis error");
+redis.on("connect", () => {
+  console.log("Connected to Redis Cloud via TLS");
 });
 
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
 
 export default redis;
-
-
-
-
-// in prodcutuion 
-// import Redis from "ioredis";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const redis = new Redis({
-//   host: process.env.REDIS_HOST,
-//   port: Number(process.env.REDIS_PORT),
-//   username: process.env.REDIS_USER,
-//   password: process.env.REDIS_PASSWORD,
-// });
-
-// redis.on("connect", () => {
-//   console.log("Connected to Redis Cloud via TLS");
-// });
-
-// redis.on("error", (err) => {
-//   console.error("Redis connection error:", err);
-// });
-
-// export default redis;
